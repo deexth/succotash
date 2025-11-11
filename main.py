27769437e6d4883848c121059a42ff0ee5d264dd
deexth <hkabare@gmail.com>
2025-11-11 20:21:24 +0100

    api call

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
+def gemini_call(key, prompt):
+    client = genai.Client(api_key=key)
+
+    messages = [
+        genai.types.Content(role="user", parts=[genai.types.Part(text=prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+
+    return response
+
+
 def main():
-    print("Hello from succotash!")
+    if len(sys.argv) < 2:
+        print("You need to provide an argument for the prompt")
+        sys.exit(1)
+    prompt = sys.argv[1]
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    resp = gemini_call(api_key, prompt)
+
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        print(f"Response: {resp.text}")
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
+    else:
+        print(resp.text)
 
 
 if __name__ == "__main__":
